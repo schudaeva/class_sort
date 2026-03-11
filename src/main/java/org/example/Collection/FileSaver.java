f057796b200d3cab492968b43421bb6ed586581e
artemka2003 <86artem2003@mail.ru>
2026-03-11 20:51:59 +0300

    В первом коммите забыл FileSaver изменить, поправляю

--- src/main/java/org/example/Collection/FileSaver.java
+++ src/main/java/org/example/Collection/FileSaver.java
@@ -12,3 +12,45 @@ public class FileSaver {
     }
 
 }
+
+// мой коммит:
+/*
+package Collection;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class FileSaver {
+
+    public <T> boolean appendValues(Path path, List<T> values) {
+        return write(path, values,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND);
+    }
+
+    public <T> boolean writeValues(Path path, List<T> values) {
+        return write(path, values,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    private <T> boolean write(Path path, List<T> values, StandardOpenOption... options) {
+        try {
+            List<String> lines = values.stream()
+                    .map(String::valueOf)
+                    .toList();
+
+            Files.write(path, lines, options);
+            return true;
+        } catch (IOException e) {
+            // Здесь типа должно быть логирование, например через logback, но мне пока рано это изучать
+            return false;
+        }
+    }
+
+
+}
+*/
