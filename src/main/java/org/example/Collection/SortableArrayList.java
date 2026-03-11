ea1c32a2ae8d26ced51088f32935b67e09637c07
artemka2003 <86artem2003@mail.ru>
2026-03-11 20:47:59 +0300

    Первая реализация коллекции на самостоятельном классе Car, и первый коммит записи в файл

--- src/main/java/org/example/Collection/SortableArrayList.java
+++ src/main/java/org/example/Collection/SortableArrayList.java
@@ -40,4 +40,53 @@ public class SortableArrayList implements SortableCollection {
     public Iterator<Sortable> iterator() {
         return null;
     }
+
 }
+
+// my commit:
+/**
+
+import Entity.Car;
+
+import java.util.*;
+
+public class CarArrayList {
+    private Car[] elements;
+    private int size;
+
+
+    public CarArrayList() {
+        elements = new Car[10];
+    }
+
+
+    public void add(Car car){
+        if (size == elements.length) {
+            Car[] newArray = new Car[elements.length * 2];
+            System.arraycopy(elements, 0, newArray, 0, elements.length);
+            elements = newArray;
+        }
+        elements[size] = car;
+        size++;
+
+    }
+
+    public Car get(int index){
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("This collection hasn't this index!");
+        }
+        return elements[index];
+    }
+
+
+    public int size(){
+        return size;
+    }
+
+    public Car[] toArray(){
+        Car[] result = new Car[size];
+        System.arraycopy(elements, 0, result, 0, size);
+        return result;
+    }
+
+}*/
