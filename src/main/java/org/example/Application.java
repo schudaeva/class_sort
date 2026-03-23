f3843a4c16ffba7615d682aebb6e4f6b49c473d3
schudaeva <koygeri@mail.ru>
2026-03-23 23:13:18 +0300

    -

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -9,26 +9,27 @@ import org.example.util.Menu;
 public class Application {
     public static void main(String[] args) {
 
-        JsonManager json = new JsonManager(Car.class, SortableArrayList::new, "fileJ.json") {};
-        json.clear();
-
-        SortableCollection<Car> cars = new SortableArrayList<>();
-        cars.add(new Car.Builder().power(110).model("рено").year(1999).build());
-        cars.add(new Car.Builder().power(1312).model("lambubu").year(2000).build());
-        SortableCollection<Car> cars2 = new SortableArrayList<>();
-        cars2.add(new Car.Builder().power(120).model("пежо").year(1914).build());
-        cars2.add(new Car.Builder().power(103).model("солярий").year(2018).build());
-
-
-        json.append(cars);
-        json.append(cars2);
-
-        for (int i = 0; i < json.loadArray().length; i++) {
-            System.out.println(json.loadArray()[i]);
-        }
-        System.out.println(json.load());
-
-
+//        JsonManager json = new JsonManager(Car.class, SortableArrayList::new, "fileJ.json") {};
+//        json.clear();
+//
+//        SortableCollection<Car> cars = new SortableArrayList<>();
+//        cars.add(new Car.Builder().power(110).model("рено").year(1999).build());
+//        cars.add(new Car.Builder().power(1312).model("lambubu").year(2000).build());
+//        SortableCollection<Car> cars2 = new SortableArrayList<>();
+//        cars2.add(new Car.Builder().power(120).model("пежо").year(1914).build());
+//        cars2.add(new Car.Builder().power(103).model("солярий").year(2018).build());
+//
+//
+//        json.append(cars);
+//        json.append(cars2);
+//
+//
+//        for (int i = 0; i < json.loadArray().length; i++) {
+//            System.out.println(json.loadArray()[i]);
+//        }
+//        System.out.println(json.load());
+//
+//
 
 
         Menu menu = new Menu();
